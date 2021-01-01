4bb5187a47bcca54bee404b291b10a8eee19f556
Diego Doná <diedona@gmail.com>
2021-01-01 18:24:14 -0300

    mudanças de dev

--- Web/WebApp/Controllers/HomeController.cs
+++ Web/WebApp/Controllers/HomeController.cs
@@ -28,6 +28,11 @@ namespace WebApp.Controllers
             return View();
         }
 
+        public IActionResult Dev()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
